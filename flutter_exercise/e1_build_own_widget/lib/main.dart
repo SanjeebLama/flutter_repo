6808e549bcb28eb33f5aa91816586e5e52aad9f0
S J <sanjeeb.lama004@gmail.com>
2020-07-27 21:42:38 +0545

    e1 completed

--- flutter_exercise/e1_build_own_widget/lib/main.dart
+++ flutter_exercise/e1_build_own_widget/lib/main.dart
@@ -1,5 +1,36 @@
 import 'package:flutter/material.dart';
 
+import 'package:e1_build_own_widget/category.dart';
+
+// TODO: Pass this information into your custom [Category] widget
+const _categoryName = 'Cake';
+const _categoryIcon = Icons.cake;
+const _categoryColor = Colors.green;
+
+/// The function that is called when main.dart is run.
 void main() {
-  // runApp();
+  runApp(UnitConverterApp());
+}
+
+/// This widget is the root of our application.
+/// Currently, we just show one widget in our app.
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: Scaffold(
+        backgroundColor: Colors.green[100],
+        body: Center(
+          // TODO: Determine what properties you'll need to pass into the widget
+          child: Category(
+            name: _categoryName,
+            color: _categoryColor,
+            iconLocation: _categoryIcon,
+          ),
+        ),
+      ),
+    );
+  }
 }

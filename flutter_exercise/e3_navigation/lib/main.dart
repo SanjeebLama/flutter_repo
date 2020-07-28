bf30d44bbbf1c6f5a93c1f9a7072ebd5f9677134
S J <sanjeeb.lama004@gmail.com>
2020-07-28 16:22:34 +0545

    e3 completed

---
+++ flutter_exercise/e3_navigation/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// TODO: Import the CategoryRoute widget
+import 'package:e3_navigation/category_route.dart';
+
+/// The function that is called when main.dart is run.
+void main() {
+  runApp(UnitConverterApp());
+}
+
+/// The first screen we see is a list [Categories].
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      // TODO: Instead of pointing to exactly 1 Category widget,
+      // our home should now point to an instance of the CategoryRoute widget.
+      home: CategoryRoute(),
+    );
+  }
+}

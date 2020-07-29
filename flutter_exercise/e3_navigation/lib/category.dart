00e0c954cf90095351e4ed248c74d6f1a1f6b85c
S J <sanjeeb.lama004@gmail.com>
2020-07-29 12:57:41 +0545

    category updated

--- flutter_exercise/e3_navigation/lib/category.dart
+++ flutter_exercise/e3_navigation/lib/category.dart
@@ -102,7 +102,7 @@ class Category extends StatelessWidget {
                   child: Text(
                     name,
                     textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.headline,
+                    style: Theme.of(context).textTheme.headline5,
                   ),
                 ),
               ],

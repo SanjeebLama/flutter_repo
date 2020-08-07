b339031f8d94dd28d4adea1521fd760c9ffd0667
S J <sanjeeb.lama004@gmail.com>
2020-08-07 21:53:02 +0545

    Feature added

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,43 +1,16 @@
 import 'package:flutter/material.dart';
+// import 'entrance_fader.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(CardsExample());
 
-class MyApp extends StatelessWidget {
+class CardsExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: Colors.purple),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('No Worries'),
-        ),
-        body: Screen(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
       ),
-    );
-  }
-}
-
-class Screen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          'Login Form',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-        // TextField(
-        //   decoration: InputDecoration(
-        //       labelText: 'Email',
-        //       hintText: 'you@email.com',
-        //       icon: Icon(Icons.contact_mail)),
-        // ),
-      ],
+      body: Text('Hello World'),
     );
   }
 }
